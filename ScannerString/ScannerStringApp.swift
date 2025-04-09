9fb74d42476c86193842c19f3c02a6a31a29de94
aSynch1889 <9366321@gmail.com>
2025-04-10 00:39:12 +0800

    初始化项目

---
+++ ScannerString/ScannerStringApp.swift
@@ -0,0 +1,19 @@
+//
+//  ScannerStringApp.swift
+//  ScannerString
+//
+//  Created by 华子 on 2025/4/10.
+//
+
+import SwiftUI
+
+@main
+struct ScannerStringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(minWidth: 800, minHeight: 600)
+        }
+        .windowStyle(.hiddenTitleBar)
+    }
+}
